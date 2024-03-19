954579ae538aa541ae98910948f68a63c570055f
John Cherian <john.jcheri@gmail.com>
2024-03-19 09:10:58 -0700

    logger added to name sampler and some more error handling

--- src/data_utils/sample_names.py
+++ src/data_utils/sample_names.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np
 import requests
 
@@ -6,9 +7,15 @@ from tqdm import tqdm
 
 from concurrent.futures import ThreadPoolExecutor
 
+ENTITY_PATH = '/data/jcherian/wikipedia_entity_map.npz'
 WIKIDATA_URL = "https://www.wikidata.org/w/api.php"
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='human.log', level=logging.INFO)
+
 
 def get_id(response : Dict) -> str:
+    if response.get("entities", None) is None:
+        return None
     wikidata_codes = list(response['entities'].keys())
     assert len(wikidata_codes) == 1
     return wikidata_codes[0]
@@ -23,36 +30,57 @@ def is_human(response : Dict, id: str) -> bool:
 
 def validate_entity(k):
     name = k.split('/')[-1]
-    response = requests.get(url=WIKIDATA_URL, params={"action" : "wbgetentities",
-                                                    "sites" : "enwiki",
-                                                    "titles" : name,
-                                                    "normalize": "1",
-                                                    "languages": "en",
-                                                    "format": "json",
-                                                    "props": "claims"})
-    response = response.json()
+    adapter = requests.adapters.HTTPAdapter(max_retries=10)
+    with requests.session() as s:
+        s.mount("https://", adapter)
+        response = s.get(url=WIKIDATA_URL, params={"action" : "wbgetentities",
+                                                        "sites" : "enwiki",
+                                                        "titles" : name,
+                                                        "normalize": "1",
+                                                        "languages": "en",
+                                                        "format": "json",
+                                                        "props": "claims"})
+
+    try:
+        response = response.json()
+    except:
+        print(response.text)
 
     wiki_id = get_id(response)
+    
+    if wiki_id is None:
+        return name, False
 
     try:
         human = is_human(response, wiki_id)
     except:
-        print(f"Failed {name}.")
         return name, False
+    logger.info(f"{name}, {human}")
     return name, human
 
 
 if __name__ == "__main__":
-    wiki_entities = np.load('/Users/cherian/Projects/pretraining_entities/wikipedia_entity_map.npz')
-    with ThreadPoolExecutor(max_workers=25) as executor:
-        res = list(
-            tqdm(
-                executor.map(
-                    lambda k : validate_entity(k),
-                    wiki_entities.keys()
-                ),
-                total=len(wiki_entities)
+    wiki_entities = np.load(ENTITY_PATH)
+    entity_names = list(wiki_entities.keys())
+    try:
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            res = list(
+                tqdm(
+                    executor.map(
+                        lambda k : validate_entity(k),
+                        entity_names
+                    ),
+                    total=len(entity_names)
+                )
             )
-        )
+    except:
+        import pickle
+        with open('human.pkl', 'wb') as fp:
+            pickle.dump(res, fp)
+    
+
+    import pickle
+    with open('human.pkl', 'wb') as fp:
+        pickle.dump(res, fp)
 
     import IPython; IPython.embed()
